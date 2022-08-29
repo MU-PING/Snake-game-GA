47ad9c386cb54f11c72eb6a9a99ed4955d032c56
Muping <k0326jim@gmail.com>
2022-08-29 19:16:44 +0800

    150 generation

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from generationEvolution import *
 
 if __name__ == '__main__':
     brainNum = 400
-    generations = 400
+    generations = 150
 
     print("Initializing generation...")
     evolution = Evolution(brainNum, generations)
